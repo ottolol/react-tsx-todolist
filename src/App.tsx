2b88ea82f5c35eab0b8294f7fdc2b2504da264ab
haka <alexsveshnik@gmail.com>
2023-06-15 22:35:43 +0300

    не получается назнавчить тип данных для массивов task1 и task2

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Todolist } from './Todolist';
+import { PropsType } from './Todolist';
 
 function App() {
+
+  let task1: PropsType = [
+    { id: 1, title: "CSS@HTML", isDone: true },
+    { id: 2, title: "JS", isDone: true },
+    { id: 3, title: "React", isDone: false },
+  ]
+
+  let task2: PropsType = [
+    { id: 1, title: "Terminator", isDone: true },
+    { id: 2, title: "Avatar", isDone: true },
+    { id: 3, title: "Transformers", isDone: true },
+  ]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <header className="App-header"> */}
+      <Todolist title="What to learn" tasks={task1} />
+      <Todolist title="Movies" tasks={task2} />
+      {/* </header> */}
     </div>
   );
 }
