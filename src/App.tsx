d83f210f3a24d711d421149310a58b7504c0a432
haka <alexsveshnik@gmail.com>
2023-06-18 12:30:16 +0300

    реализовали фильтр тасок - all, completed, active

--- src/App.tsx
+++ src/App.tsx
@@ -2,32 +2,46 @@ import React, { useState } from 'react';
 import './App.css';
 import { TaskType, Todolist } from './Todolist';
 
+export type FilterValuesType = "all" | "completed" | "active";
+
 function App() {
 
-  let initialTasks: Array<TaskType> = [
+  // кладем в useState наш массив тасок
+  let [tasks, setTasks] = useState<Array<TaskType>>([
     { id: 1, title: "CSS@HTML", isDone: true },
     { id: 2, title: "JS", isDone: true },
     { id: 3, title: "React", isDone: false },
-    { id: 4, title: "Redux", isDone: false },
-  ]
-
-  // кладем в useState наш массив тасок - initialTasks
-  const [tasks, setTasks] = useState(initialTasks);
+    { id: 4, title: "Redux", isDone: false }
+  ]);
+  let [filter, setFilter] = useState<FilterValuesType>("all");
 
   // Удаляем таски при нажатии на кнопку - x
   function removeTask(id: number) {
-    // фильтруем массив тасок - initialTasks, он же tasks
+    // фильтруем массив тасок tasks
     let filteredTasks = tasks.filter(t => t.id !== id)
     // Присваиваем setTasks новый, отфильтрованный массив тасок - filteredTasks
     setTasks(filteredTasks)
+  }
+
+  function changeFilter(value: FilterValuesType) {
+    setFilter(value);
+  }
+
+  let tasksForTodolist = tasks;
+  if (filter === "completed") {
+    tasksForTodolist = tasks.filter(t => t.isDone === true);
+  }
+  if (filter === "active") {
+    tasksForTodolist = tasks.filter(t => t.isDone === false);
   }
 
   return (
     <div className="App">
       <Todolist
         title="What to learn"
-        tasks={tasks}
+        tasks={tasksForTodolist}
         removeTask={removeTask}
+        changeFilter={changeFilter}
       />
     </div>
   );
